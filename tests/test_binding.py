31e017af2fe732d7314a524ab63568a65ab02e69
Tommy Carpenter <tommy@research.att.com>
2018-03-05 11:25:44 -0500

    Enforce most pep8 compliance in tox
    Also fix some pep8 failings
    
    Change-Id: I6904c171c898d510ac2b7e2435b5a80dcdf1be13
    Issue-ID: DCAEGEN2-348
    Signed-off-by: Tommy Carpenter <tommy@research.att.com>

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -111,7 +111,7 @@ def test_bind_config_for_scn(monkeypatch):
         client.resolve("scn_NOTexists")
 
     R = controller.bind_config_for_scn("scn_exists")
-    assert(json.loads(R.data) == {"foo3" : "bar3"})
+    assert(json.loads(R.data) == {"foo3": "bar3"})
     assert(R.status_code == 200)
 
     R = controller.bind_config_for_scn("scn_NOTexists")
@@ -140,22 +140,25 @@ def test_generic(monkeypatch):
 
 
 def test_bad_config_http():
-    test_config = {'yeahhhhh' : "{{}}"}
+    test_config = {'yeahhhhh': "{{}}"}
     test_rels = ["testing_bravo.somedomain.com"]
-    assert {'yeahhhhh' : []} == client.resolve_override(test_config, test_rels)
+    assert {'yeahhhhh': []} == client.resolve_override(test_config, test_rels)
+
 
 def test_bad_config_dmaap():
-    test_config = {'darkness' : "<<>>"}
-    test_dmaap = {"WHO?" : "darkness"}
-    assert {'darkness' : {}} == client.resolve_override(test_config, test_dmaap)
+    test_config = {'darkness': "<<>>"}
+    test_dmaap = {"WHO?": "darkness"}
+    assert {'darkness': {}} == client.resolve_override(test_config, test_dmaap)
+
 
 def test_config(monkeypatch):
     #test config override
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
-    test_config = {"autoderegisterafter": "10m", "cdap_to_manage": {'some_nested_thing' : "{{cdap}}"}, "bindingttw": 5, "hcinterval": "5s"}
+    test_config = {"autoderegisterafter": "10m", "cdap_to_manage": {'some_nested_thing': "{{cdap}}"}, "bindingttw": 5, "hcinterval": "5s"}
     test_rels = ["cdap"]
     test_bind_1 = client.resolve_override(test_config, test_rels)
     assert test_bind_1 == {'autoderegisterafter': '10m', 'cdap_to_manage': {'some_nested_thing': ['666.666.666.666:666']}, 'bindingttw': 5, 'hcinterval': '5s'}
+
 
 def test_config_with_list(monkeypatch):
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
@@ -164,39 +167,42 @@ def test_config_with_list(monkeypatch):
     test_bind_1 = client.resolve_override(test_config_1, test_rels_1, {})
     assert(test_bind_1 == {'dcae_target_type': ['vhss-ems', 'pcrf-oam'], 'downstream-laika': [], 'some-param': 'Lorem ipsum dolor sit amet'})
 
-    test_config_2 = {"foo" : ["{{cdap}}", "notouching", "<<yo>>"]}
+    test_config_2 = {"foo": ["{{cdap}}", "notouching", "<<yo>>"]}
     test_rels_2 = ["cdap"]
-    test_dmaap_2={"yo" : "im here"}
+    test_dmaap_2 = {"yo": "im here"}
     test_bind_2 = client.resolve_override(test_config_2, test_rels_2, test_dmaap_2)
-    assert(test_bind_2 == {"foo" : [['666.666.666.666:666'], "notouching", "im here"]})
+    assert(test_bind_2 == {"foo": [['666.666.666.666:666'], "notouching", "im here"]})
+
 
 def test_non_existent(monkeypatch):
     #test a valid config-rels but the key is not in Consul
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
-    test_config = {"you shall not be fufilled" : "{{nonexistent_hope}}"}
+    test_config = {"you shall not be fufilled": "{{nonexistent_hope}}"}
     test_rels = ["nonexistent_hope.rework-central.ecomp.somedomain.com"] #hopefully not registered in Consul..
     test_bind_1 = client.resolve_override(test_config, test_rels, {})
-    assert(test_bind_1 == {"you shall not be fufilled" : []})
+    assert(test_bind_1 == {"you shall not be fufilled": []})
+
 
 def test_cdap(monkeypatch):
     #user override to test CDAP functionality
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
     test_rels = ["testing_alpha.somedomain.com", "testing_bravo.somedomain.com", "testing_charlie.somedomain.com", "testing_charlie.somedomain.com", "cdap"]
-    test_config = { "streams_publishes" : "{{alpha}}",
-                    "services_calls" : [{"somekey" : "{{charlie}}"}], #should be dumped
-                    "cdap_to_manage": {'some_nested_thing' : "{{cdap}}"} #no dumps
-                  }
+    test_config = {"streams_publishes": "{{alpha}}",
+                   "services_calls": [{"somekey": "{{charlie}}"}], #should be dumped
+                   "cdap_to_manage": {'some_nested_thing': "{{cdap}}"}} #no dumps
     test_bind_1 = client.resolve_override(test_config, test_rels)
     assert test_bind_1 == {'services_calls': [{"somekey": ["5.5.5.5:555", "5.5.5.5:555"]}], 'streams_publishes': ["6.6.6.6:666"], 'cdap_to_manage': {'some_nested_thing': ['666.666.666.666:666']}}
-    assert test_bind_1['services_calls'] == [{"somekey" : ["5.5.5.5:555", "5.5.5.5:555"]}]
+    assert test_bind_1['services_calls'] == [{"somekey": ["5.5.5.5:555", "5.5.5.5:555"]}]
     assert test_bind_1['streams_publishes'] == ["6.6.6.6:666"]
+
 
 def test_broker_redirect(monkeypatch):
     #test the broker redirect
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
-    test_config = {"gimmie_dat_cdap" : "{{cdap_serv}}"}
+    test_config = {"gimmie_dat_cdap": "{{cdap_serv}}"}
     test_rels = ["cdap_serv.dcae.ecomp.somedomain.com"]
-    assert {"gimmie_dat_cdap" : ['http://1.1.1.1:444/application/cdap_serv.dcae.ecomp.somedomain.com']} == client.resolve_override(test_config, test_rels)
+    assert {"gimmie_dat_cdap": ['http://1.1.1.1:444/application/cdap_serv.dcae.ecomp.somedomain.com']} == client.resolve_override(test_config, test_rels)
+
 
 def test_multiple_service_types(monkeypatch):
     #test {{x,y,z}}
@@ -204,73 +210,66 @@ def test_multiple_service_types(monkeypatch):
 
     #test 1: they all resovle
     test_rels = ["testing_alpha.somedomain.com", "testing_bravo.somedomain.com", "testing_charlie.somedomain.com", "testing_charlie.somedomain.com"]
-    config = {"ALL YOUR SERVICE BELONG TO US" : "{{alpha,bravo,charlie}}"}
+    config = {"ALL YOUR SERVICE BELONG TO US": "{{alpha,bravo,charlie}}"}
     test_bind_1 = client.resolve_override(config, test_rels)
-    assert(test_bind_1 == {"ALL YOUR SERVICE BELONG TO US" : ['6.6.6.6:666', '7.7.7.7:777', '5.5.5.5:555', '5.5.5.5:555']})
+    assert(test_bind_1 == {"ALL YOUR SERVICE BELONG TO US": ['6.6.6.6:666', '7.7.7.7:777', '5.5.5.5:555', '5.5.5.5:555']})
 
     #test 2: two resolve, one is missing from rels key
-    config2 = {"two there one not exist" : "{{alpha,bravo,notexist}}"}
+    config2 = {"two there one not exist": "{{alpha,bravo,notexist}}"}
     test_bind_2 = client.resolve_override(config2, test_rels)
-    assert(test_bind_2 == {"two there one not exist" : ['6.6.6.6:666', '7.7.7.7:777']})
+    assert(test_bind_2 == {"two there one not exist": ['6.6.6.6:666', '7.7.7.7:777']})
 
     #test 3: two resolve, one is in rels key but not registered
-    config3 = {"two there one unregistered" : "{{alpha,bravo,unregistered}}"}
-    test_rels3 =  ["testing_alpha.somedomain.com", "testing_bravo.somedomain.com", "unregistered.somedomain.com"]
+    config3 = {"two there one unregistered": "{{alpha,bravo,unregistered}}"}
+    test_rels3 = ["testing_alpha.somedomain.com", "testing_bravo.somedomain.com", "unregistered.somedomain.com"]
     test_bind_3 = client.resolve_override(config3, test_rels3)
-    assert(test_bind_3 == {"two there one unregistered" : ['6.6.6.6:666', '7.7.7.7:777']})
+    assert(test_bind_3 == {"two there one unregistered": ['6.6.6.6:666', '7.7.7.7:777']})
+
 
 def test_dmaap(monkeypatch):
     #test resolving dmaap key
-    config = {"TODAY IS YOUR LUCKY DAY" : "<<XXX>>"}
+    config = {"TODAY IS YOUR LUCKY DAY": "<<XXX>>"}
     #does not match
-    test_bind = client.resolve_override(config, dmaap={"XX" : "ABSOLVEME"}) #XX != XXX
-    assert(test_bind == {"TODAY IS YOUR LUCKY DAY" : {}})
+    test_bind = client.resolve_override(config, dmaap={"XX": "ABSOLVEME"}) #XX != XXX
+    assert(test_bind == {"TODAY IS YOUR LUCKY DAY": {}})
     #matches
-    test_bind_2 = client.resolve_override(config, dmaap={"XXX" : "ABSOLVEME"})
-    assert(test_bind_2 == {"TODAY IS YOUR LUCKY DAY" : "ABSOLVEME"})
+    test_bind_2 = client.resolve_override(config, dmaap={"XXX": "ABSOLVEME"})
+    assert(test_bind_2 == {"TODAY IS YOUR LUCKY DAY": "ABSOLVEME"})
 
-expected_config = {
-                "deep" : {
-                    "ALL YOUR SERVICE BELONG TO US" : ['6.6.6.6:666', '7.7.7.7:777', '5.5.5.5:555', '5.5.5.5:555']},
-                "doubledeep" :  {
-                    "sodeep" : {"hello" : "darkness"}}
-             }
+
+expected_config = {"deep": {"ALL YOUR SERVICE BELONG TO US": ['6.6.6.6:666', '7.7.7.7:777', '5.5.5.5:555', '5.5.5.5:555']},
+                   "doubledeep": {"sodeep": {"hello": "darkness"}}}
+
 
 def test_both(monkeypatch):
     #test rels and http
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
     test_rels = ["testing_alpha.somedomain.com", "testing_bravo.somedomain.com", "testing_charlie.somedomain.com", "testing_charlie.somedomain.com"]
-    test_dmaap = {"WHO?" : "darkness"}
-    config = {
-                "deep" : {
-                    "ALL YOUR SERVICE BELONG TO US" : "{{alpha,bravo,charlie}}"},
-                "doubledeep" :  {
-                    "sodeep" : {"hello" : "<<WHO?>>"}}
-             }
+    test_dmaap = {"WHO?": "darkness"}
+    config = {"deep": {"ALL YOUR SERVICE BELONG TO US": "{{alpha,bravo,charlie}}"},
+              "doubledeep": {"sodeep": {"hello": "<<WHO?>>"}}}
     test_bind_1 = client.resolve_override(config, test_rels, test_dmaap)
     assert(test_bind_1 == expected_config)
 
 
 def test_resolve_all(monkeypatch):
     monkeypatch.setattr('requests.put', monkeyed_requests_put)
-    allk =  client.resolve_all("test_service_component_name.unknown.unknown.unknown.dcae.onap.org")
+    allk = client.resolve_all("test_service_component_name.unknown.unknown.unknown.dcae.onap.org")
     withstuff = {'config': {'my': 'amazing config'},
-                    'dti': {'my': 'dti'},
-                    'policies': {'items': [{'policyName': 'DCAE_alex.Config_MS_alex_microservice.132.xml', 'policyConfigMessage': 'Config Retrieved! ', 'responseAttributes': {}, 'policyConfigStatus': 'CONFIG_RETRIEVED', 'matchingConditions': {'ONAPName': 'DCAE', 'Name': 'DCAE', 'ConfigName': 'alex_config_name'}, 'config': {'policyScope': 'alex_policy_scope', 'configName': 'alex_config_name', 'description': 'test DCAE policy-handler', 'service': 'alex_service', 'policyName': 'alex_policy_name', 'riskLevel': '3', 'key1': 'value1', 'policy_hello': 'world!', 'content': {'foo': 'microservice3', 'foo_updated': '2018-01-30T13:25:33.222Z'}, 'riskType': '1712_ETE', 'guard': 'False', 'version': '0.0.1', 'location': 'Central', 'policy_updated_ts': '2018-02-19T15:09:55.217Z', 'updated_policy_id': 'DCAE_alex.Config_MS_alex_microservice', 'policy_updated_to_ver': '132', 'priority': '4', 'policy_updated_from_ver': '131', 'templateVersion': '2', 'uuid': '5e87d7c5-0daf-4b6b-ab92-5365cf5db1ef'}, 'property': None, 'type': 'JSON', 'policyVersion': '132'}, {'policyName': 'DCAE_alex.Config_db_client_policy_id_value.133.xml', 'policyConfigMessage': 'Config Retrieved! ', 'responseAttributes': {}, 'policyConfigStatus': 'CONFIG_RETRIEVED', 'matchingConditions': {'ONAPName': 'DCAE', 'Name': 'DCAE', 'ConfigName': 'alex_config_name'}, 'config': {'db_client_ts': '2017-11-21T12:12:13.696Z', 'db_client': 'ipsum', 'policy_hello': 'world!', 'policy_updated_from_ver': '132', 'updated_policy_id': 'DCAE_alex.Config_db_client_policy_id_value', 'policy_updated_ts': '2018-02-19T15:09:55.812Z', 'policy_updated_to_ver': '133'}, 'property': None, 'type': 'JSON', 'policyVersion': '133'}], 'event': {'action': 'gathered', 'timestamp': '2018-02-19 15:36:44.877380', 'update_id': 'bb73c20a-5ff8-450f-8223-da6720ade267', 'policies_count': 2}}
-                   }
+                 'dti': {'my': 'dti'},
+                 'policies': {'items': [{'policyName': 'DCAE_alex.Config_MS_alex_microservice.132.xml', 'policyConfigMessage': 'Config Retrieved! ', 'responseAttributes': {}, 'policyConfigStatus': 'CONFIG_RETRIEVED', 'matchingConditions': {'ONAPName': 'DCAE', 'Name': 'DCAE', 'ConfigName': 'alex_config_name'}, 'config': {'policyScope': 'alex_policy_scope', 'configName': 'alex_config_name', 'description': 'test DCAE policy-handler', 'service': 'alex_service', 'policyName': 'alex_policy_name', 'riskLevel': '3', 'key1': 'value1', 'policy_hello': 'world!', 'content': {'foo': 'microservice3', 'foo_updated': '2018-01-30T13:25:33.222Z'}, 'riskType': '1712_ETE', 'guard': 'False', 'version': '0.0.1', 'location': 'Central', 'policy_updated_ts': '2018-02-19T15:09:55.217Z', 'updated_policy_id': 'DCAE_alex.Config_MS_alex_microservice', 'policy_updated_to_ver': '132', 'priority': '4', 'policy_updated_from_ver': '131', 'templateVersion': '2', 'uuid': '5e87d7c5-0daf-4b6b-ab92-5365cf5db1ef'}, 'property': None, 'type': 'JSON', 'policyVersion': '132'}, {'policyName': 'DCAE_alex.Config_db_client_policy_id_value.133.xml', 'policyConfigMessage': 'Config Retrieved! ', 'responseAttributes': {}, 'policyConfigStatus': 'CONFIG_RETRIEVED', 'matchingConditions': {'ONAPName': 'DCAE', 'Name': 'DCAE', 'ConfigName': 'alex_config_name'}, 'config': {'db_client_ts': '2017-11-21T12:12:13.696Z', 'db_client': 'ipsum', 'policy_hello': 'world!', 'policy_updated_from_ver': '132', 'updated_policy_id': 'DCAE_alex.Config_db_client_policy_id_value', 'policy_updated_ts': '2018-02-19T15:09:55.812Z', 'policy_updated_to_ver': '133'}, 'property': None, 'type': 'JSON', 'policyVersion': '133'}], 'event': {'action': 'gathered', 'timestamp': '2018-02-19 15:36:44.877380', 'update_id': 'bb73c20a-5ff8-450f-8223-da6720ade267', 'policies_count': 2}}}
     assert allk == withstuff
 
     monkeypatch.setattr('config_binding_service.client._get_connection_info_from_consul', monkeyed_get_connection_info_from_consul)
     allk = client.resolve_all("test_resolve_scn")
-    print(allk)
-    assert allk == {"config" : expected_config}
+    assert allk == {"config": expected_config}
 
     R = controller.bind_all("test_service_component_name.unknown.unknown.unknown.dcae.onap.org")
     assert(json.loads(R.data) == withstuff)
     assert(R.status_code == 200)
 
     R = controller.bind_all("test_resolve_scn")
-    assert(json.loads(R.data) == {"config" : expected_config})
+    assert(json.loads(R.data) == {"config": expected_config})
     assert(R.status_code == 200)
 
     R = controller.bind_all("scn_NOTexists")
@@ -278,4 +277,3 @@ def test_resolve_all(monkeypatch):
 
     R = controller.bind_all("asdfasdf")
     assert(R.status_code == 500)
-
