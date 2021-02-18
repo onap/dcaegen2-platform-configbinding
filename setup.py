a238849dee5841ea424a8154319e688ceb7bad5c
Alexander Mazuruk <a.mazuruk@samsung.com>
2021-02-18 17:16:29 +0100

    Change python baseOS img to integration-python
    Switched to latest version (current seccom recommndation)
    
    Benefits from switching over:
    * minimal {java11,python} images maintained by integration team
    * using currently "blessed by seccom" versions (:latest tag used)
    * should limit spread of legal issues across layers
    * integration images will be the first to have automated compliance
      documentation
    * should limit spread of base layers (contributing to deployment
      footprint - more base layers = more to download, more to store etc...)
    
    Issue-ID: INT-1864
    Issue-ID: DCAEGEN2-2420
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>
    Change-Id: Ia0fec819dc4f83f4ca177f40cc012f9359da4bdc

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="config_binding_service",
-    version="2.5.3",
+    version="2.5.4",
     packages=find_packages(exclude=["tests.*", "tests"]),
     author="Tommy Carpenter",
     author_email="tommy@research.att.com",
